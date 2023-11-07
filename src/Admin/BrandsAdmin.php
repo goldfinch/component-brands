3e276995d92c01a3d93572f89c564696ff6066eb
Art Karasev <as.karasyov@gmail.com>
2023-11-08 08:10:30 +1300

    components

---
+++ src/Admin/BrandsAdmin.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Goldfinch\Component\Brands\Admin;
+
+use Goldfinch\Component\Brands\Models\BrandItem;
+use Goldfinch\Component\Brands\Blocks\BrandsBlock;
+use Goldfinch\Component\Brands\Configs\BrandConfig;
+use SilverStripe\Admin\ModelAdmin;
+use JonoM\SomeConfig\SomeConfigAdmin;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+
+class BrandsAdmin extends ModelAdmin
+{
+    use SomeConfigAdmin;
+
+    private static $url_segment = 'brands';
+    private static $menu_title = 'Brands';
+    private static $menu_icon_class = 'bi-google-play';
+    // private static $menu_priority = -0.5;
+
+    private static $managed_models = [
+        BrandItem::class => [
+            'title'=> 'Brands',
+        ],
+        BrandsBlock::class => [
+            'title'=> 'Blocks',
+        ],
+        BrandConfig::class => [
+            'title'=> 'Settings',
+        ],
+    ];
+
+    // public $showImportForm = true;
+    // public $showSearchForm = true;
+    // private static $page_length = 30;
+
+    public function getList()
+    {
+        $list = parent::getList();
+
+        // ..
+
+        return $list;
+    }
+
+    protected function getGridFieldConfig(): GridFieldConfig
+    {
+        $config = parent::getGridFieldConfig();
+
+        // ..
+
+        return $config;
+    }
+
+    public function getSearchContext()
+    {
+        $context = parent::getSearchContext();
+
+        // ..
+
+        return $context;
+    }
+
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
+
+        // ..
+
+        return $form;
+    }
+
+    // public function getExportFields()
+    // {
+    //     return [
+    //         // 'Name' => 'Name',
+    //         // 'Category.Title' => 'Category'
+    //     ];
+    // }
+}
---
+++ src/Models/BrandItem.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Goldfinch\Component\Brands\Models;
+
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Assets\Image;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use Goldfinch\FocusPointExtra\Forms\UploadFieldWithExtra;
+
+class BrandItem extends DataObject
+{
+    private static $table_name = 'BrandItem';
+    private static $singular_name = 'brand';
+    private static $plural_name = 'brands';
+
+    private static $db = [
+        'Name' => 'Varchar',
+        'Text' => 'HTMLText',
+        'Disabled' => 'Boolean',
+    ];
+
+    private static $has_one = [
+        'Image' => Image::class,
+    ];
+
+    private static $owns = [
+        'Image',
+    ];
+
+    // private static $belongs_to = [];
+    // private static $has_many = [];
+    // private static $belongs_many_many = [];
+    // private static $default_sort = null;
+    // private static $indexes = null;
+    // private static $casting = [];
+    // private static $defaults = [];
+
+    private static $summary_fields = [
+        'Name' => 'Name',
+        'Text.Summary' => 'Text',
+        'Disabled.NiceAsBoolean' => 'Disabled',
+    ];
+    // private static $field_labels = [];
+    // private static $searchable_fields = [];
+
+    // private static $cascade_deletes = [];
+    // private static $cascade_duplicates = [];
+
+    // * goldfinch/helpers
+    private static $field_descriptions = [];
+    private static $required_fields = [
+        'Name',
+        'Text',
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->removeByName([
+            'Image',
+            'Name',
+            'Text',
+            'Disabled',
+        ]);
+
+        // dd($fields);
+
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                ...[
+                    TextField::create('Name', 'Name'),
+                ],
+                ...UploadFieldWithExtra::create('Image', 'Image', $fields, $this)->getFields(),
+                ...[
+                    HTMLEditorField::create('Text', 'Text'),
+                    CheckboxField::create('Disabled','Disabled')->setDescription('hide this item from the list'),
+                ],
+            ]
+        );
+
+        return $fields;
+    }
+
+    // public function validate()
+    // {
+    //     $result = parent::validate();
+
+    //     // $result->addError('Error message');
+
+    //     return $result;
+    // }
+
+    // public function onBeforeWrite()
+    // {
+    //     // ..
+
+    //     parent::onBeforeWrite();
+    // }
+
+    // public function onBeforeDelete()
+    // {
+    //     // ..
+
+    //     parent::onBeforeDelete();
+    // }
+
+    // public function canView($member = null)
+    // {
+    //     return Permission::check('CMS_ACCESS_Company\Website\MyAdmin', 'any', $member);
+    // }
+
+    // public function canEdit($member = null)
+    // {
+    //     return Permission::check('CMS_ACCESS_Company\Website\MyAdmin', 'any', $member);
+    // }
+
+    // public function canDelete($member = null)
+    // {
+    //     return Permission::check('CMS_ACCESS_Company\Website\MyAdmin', 'any', $member);
+    // }
+
+    // public function canCreate($member = null, $context = [])
+    // {
+    //     return Permission::check('CMS_ACCESS_Company\Website\MyAdmin', 'any', $member);
+    // }
+}
